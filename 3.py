708e47a3bca53e134df3f65f5889db3680b5209c
klimlukichev <57773216+klimlukichev@users.noreply.github.com>
2019-11-15 00:28:42 +0300

    1

--- 3.py
+++ 3.py
@@ -1,2 +1,4 @@
-name=input("Введите ваше имя: ")
-print("Привет, ",  name.capitalize(), "!")
+a=input("Как тебя зовут: ")
+print("Привет, ",  a.capitalize(), "!")
+b=input("Как твои дела? ")
+print("Это ", b.lower(), ", когда все", b.lower(), "!")
